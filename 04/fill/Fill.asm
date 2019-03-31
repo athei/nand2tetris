4178976250b2f8c920865ee028334366b7544604
Alexander Theißen <alex.theissen@me.com>
2019-03-31 18:55:48 +0200

    Added solution for 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,46 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    @offset
+    M=0
+
+(LOOP)
+    @KBD
+    D=M
+
+    // branch -> pressed or not pressed
+    @BLACKEN
+    D;JNE
+
+    // whiten if no button pressed
+    @offset
+    D=M
+    @SCREEN
+    A=D+A
+    M=0
+    @NEXT
+    0;JMP
+
+(BLACKEN)
+    @offset
+    D=M
+    @SCREEN
+    A=D+A
+    M=-1
+
+(NEXT)
+    // increment pointer to next
+    @offset
+    MD=M+1
+    @8192
+    D=A-D
+    @LOOP
+    D;JGT
+
+(RESET)
+    @offset
+    M=0
+    @LOOP
+    0;JMP
+
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    // init output to zero
+    @R2
+    M=0
+
+    // exit early if first factor zero
+    @R0
+    D=M;
+    @END
+    D;JEQ
+
+(LOOP)
+    // add one round of second factor
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // back to top if first factor is going to zero
+    @R0
+    M=M-1
+    D=M
+    @LOOP
+    D;JNE
+
+(END)
+    @END
+    0;JMP
+
